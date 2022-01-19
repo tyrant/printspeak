806e2da553a5bd5c1f0df6b5a58e5957977a6756
Mikey Clarke <epicschnozz@gmail.com>
2022-01-20 11:56:08 +1300

    Tidied up Basket#initialize's early-return errors

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,23 +1,27 @@
 require 'csv'
+require 'item'
 
 class Basket 
 
   def initialize(input_path:)
-    raise "Can't do a thing without an input_path" if input_path.blank?
+    raise "Can't do a thing without an input_path" if input_path.nil?
     raise "input_path needs to point to an actual file" unless File.exist?(input_path)
-    raise "input_path needs to be a CSV file" unless File.extname(input_path) == '.csv'
+
     csv_headers = CSV.open(input_path, headers: true).read.headers
     unless csv_headers == ['Quantity', 'Product', 'Price']
-      raise "The CSV input file's headers must be: Quantity, Product, Price. Not: '#{csv_headers.join(', ')}' The hell's that?"
+      raise "The CSV input file's headers must be: Quantity, Product, Price. Not: '#{csv_headers.join(', ')}'. The hell's that?"
     end
 
     load_items_from(input_path: input_path)
   end
 
-
   def output
+    <<~HEREDOC
+      #{@items.map(&:output_line).join("\n")}
 
-
+      Sales Taxes: #{sprintf "%.2f", @items.map(&:line_sales_tax).sum}
+      Total: #{sprintf "%.2f", @items.map(&:line_price).sum}
+    HEREDOC
   end
 
   def load_items_from(input_path:)
